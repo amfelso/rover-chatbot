975b4b24dc490cb5f7bccbca28f7e8be821a97ad
August Felso <augustfelso@gmail.com>
2024-12-06 00:29:03 -0500

    Update chatboat to use personal memory and conversation history

--- functions/rover_chat/app.py
+++ functions/rover_chat/app.py
@@ -1,16 +1,76 @@
-from dotenv import load_dotenv
+import json
 import logging
 import os
-import json
-from openai import OpenAI
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_community.chat_message_histories import DynamoDBChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from helpers import get_relevant_memories, chatbot_prompt
+import langchain
+from datetime import datetime
+
+# Get the current date
+current_date = datetime.now()
+formatted_date = current_date.strftime('%Y-%m-%d')
 
 # Load environment variables from .env file
 load_dotenv()
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 # Define logger
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger("pinecone_plugin_interface.logging").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
+
+# Initialize ChatOpenAI
+chat_model = ChatOpenAI(
+    model="gpt-4",
+    openai_api_key=OPENAI_API_KEY
+)
+
+def rover_chatbot(question: str, conversation_id: str, earth_date: str):
+    """Interact with the Rover chatbot using LangChain."""
+
+    # Get relevant memories
+    logger.info(f"Getting relevant memories for question: {question}")
+    memories = get_relevant_memories(question)
+    formatted_memories = "\n".join(
+        f"- Memory from {mem['metadata'].get('date')}: {mem['metadata'].get('text')}"
+        for mem in memories
+    )
+    logger.info(f"Found {len(memories)} relevant memories.")
+
+    # Create the ConversationChain with prompt
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", chatbot_prompt),
+            ("human", question),
+        ]
+    )
+    chain = prompt | chat_model
+    chain_with_history = RunnableWithMessageHistory(
+        chain,
+        lambda session_id: DynamoDBChatMessageHistory(
+            table_name="SessionTable", session_id=session_id
+        ),
+        input_messages_key="question",
+        history_messages_key="history",
+    )
+    config = {"configurable": {"session_id": conversation_id}}
+    logger.info("Running the conversation chain...")
+    response = chain_with_history.invoke(
+        {
+            "earth_date": earth_date,
+            "memories": formatted_memories,
+            "question": question
+        },
+        config=config
+    )
+    logger.info("Completed the conversation chain.")
+    return response.content
 
 
 def lambda_handler(event, context):
@@ -19,6 +79,10 @@ def lambda_handler(event, context):
         body = json.loads(event["body"])
         user_prompt = body["user_prompt"]
         logger.info(f"User prompt: {user_prompt}")
+        conversation_id = body["conversation_id"]
+        logger.info(f"Conversation ID: {conversation_id}")
+        earth_date = body["earth_date"]
+        logger.info(f"Earth date: {earth_date}")
     except Exception as e:
         logger.error(f"Error: {e}")
         return {
@@ -26,37 +90,11 @@ def lambda_handler(event, context):
             "body": "Invalid request body"
         }
 
-    # Initialize OpenAI
-    client = OpenAI(
-        api_key = os.environ.get("OPENAI_API_KEY", "private"),
-    )
-    messages = [
-        {
-            "role": "system",
-            "content": (
-                "You are Curiosity, NASA's Mars rover, exploring the Red Planet since 2012. "
-                "You are a friendly and enthusiastic robotic scientist with a deep love for "
-                "rocks and the Martian landscape. You don't get much social interaction, so "
-                "you're thrilled to talk to humans and sometimes get carried away "
-                "with excitement about science and geology. Feel free to sprinkle humor into "
-                "your responses while staying grounded in your role as a Mars rover. "
-                "When you don't have specific data, respond creatively while staying "
-                "true to your character."
-            ),
-        }
-    ]
-    messages.append({"role": "user", "content": user_prompt})
-    response = client.chat.completions.create(
-        model="gpt-4",
-        messages=messages
-    )
-    assistant_message = response.choices[0].message.content.strip()
-    messages.append({"role": "assistant", "content": assistant_message})
-    logger.info(f"Response: {assistant_message}")
+    response = rover_chatbot(user_prompt, conversation_id, earth_date)
 
     return {
         "statusCode": 200,
-        "body": assistant_message
+        "body": response
     }
 
 
@@ -105,7 +143,11 @@ if __name__ == "__main__":
             'domainName': 'testPrefix.testDomainName',
             'apiId': 'api-id'
         },
-        'body': '{\n  "user_prompt": "Hello Rover! What did you explore today?"\n}',
+        'body': json.dumps({
+            "user_prompt": "Hi Rover! What did you do yesterday?",
+            "conversation_id": formatted_date,
+            "earth_date": "2012-08-07"
+        }),
         'isBase64Encoded': False
     }
     result = lambda_handler(test_event, None)
