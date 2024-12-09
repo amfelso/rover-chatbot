936893f46a164c80d9a16501748241710bf38eaa
August Felso <augustfelso@gmail.com>
2024-12-09 12:02:11 -0500

    Refactor get_logs function to improve code readability and update request validation to use query parameters

--- functions/get_logs/app.py
+++ functions/get_logs/app.py
@@ -9,11 +9,10 @@ from urllib.parse import urlparse
 
 # Get the current date
 current_date = datetime.now()
-formatted_date = current_date.strftime('%Y-%m-%d')
+formatted_date = current_date.strftime("%Y-%m-%d")
 
 # Load environment variables from .env file
 load_dotenv()
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 # Define logger
 logger = logging.getLogger()
@@ -38,15 +37,18 @@ def fetch_memory_text_boto3(s3_url):
     try:
         # Parse the S3 URL to extract bucket and key
         parsed_url = urlparse(s3_url)
-        bucket_name = parsed_url.netloc.split('.')[0] if '.s3.amazonaws.com' \
-                      in parsed_url.netloc else parsed_url.netloc
-        key = parsed_url.path.lstrip('/')
+        bucket_name = (
+            parsed_url.netloc.split(".")[0]
+            if ".s3.amazonaws.com" in parsed_url.netloc
+            else parsed_url.netloc
+        )
+        key = parsed_url.path.lstrip("/")
         logger.info(f"Bucket: {bucket_name}, Key: {key}")
 
         # Create an S3 client
-        s3_client = boto3.client('s3')
+        s3_client = boto3.client("s3")
         response = s3_client.get_object(Bucket=bucket_name, Key=key)
-        return response['Body'].read().decode('utf-8')
+        return response["Body"].read().decode("utf-8")
 
     except (NoCredentialsError, PartialCredentialsError):
         logger.error("Error: AWS credentials not found or incomplete.")
@@ -57,30 +59,23 @@ def fetch_memory_text_boto3(s3_url):
 
 
 def lambda_handler(event, context):
-    # Validate post body
-    body = json.loads(event["body"])
-    earth_date = body["earth_date"]
+    # Validate get params
+    params = event["queryStringParameters"]
+    earth_date = params["earth_date"]
     if not earth_date:
-        return {
-            "statusCode": 400,
-            "body": "Invalid request body"
-        }
+        return {"statusCode": 400, "body": "Invalid request body"}
     logger.info(f"Earth date: {earth_date}")
 
     pipeline_table = os.getenv("PIPELINE_TABLE", "PipelineTransactionLog")
 
     try:
         # Get the logs from the pipeline table in DynamoDB for the given earth date
-        dynamodb = boto3.resource('dynamodb')
+        dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table(pipeline_table)
-        response = table.get_item(
-            Key={
-                'EarthDate': earth_date
-            }
-        )['Item']
-        photos = [x['img_src'] for x in response.get("Lambda1__FetchImages")["output"]]
+        response = table.get_item(Key={"EarthDate": earth_date})["Item"]
+        photos = [x["img_src"] for x in response.get("Lambda1__FetchImages")["output"]]
         logger.info(f"Photos: {photos}")
-        
+
         memories = []
         urls = response.get("Lambda2__GenerateMemories")["output"]
         logger.info(f"Memory URLs: {urls}")
@@ -92,10 +87,7 @@ def lambda_handler(event, context):
         # Create the response in format [{"img_src": "url", "memory": "text"}]
         response = []
         for i in range(len(photos)):
-            response.append({
-                "img_src": photos[i],
-                "memory": memories[i]
-            })
+            response.append({"img_src": photos[i], "memory": memories[i]})
         logger.info(f"Response generated successfully.")
         logger.debug(f"Response: {response}")
 
@@ -103,22 +95,22 @@ def lambda_handler(event, context):
         logger.error(f"Error: {e}")
         return {
             "statusCode": 500,
-             "headers": {
-                "Access-Control-Allow-Headers" : "Content-Type",
+            "headers": {
+                "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET"
+                "Access-Control-Allow-Methods": "GET",
             },
-            "body": "Internal server error"
+            "body": "Internal server error",
         }
 
     return {
         "statusCode": 200,
         "headers": {
-            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET"
+            "Access-Control-Allow-Methods": "GET",
         },
-        "body": response
+        "body": json.dumps({"logs": response}),
     }
 
 
@@ -126,51 +118,49 @@ if __name__ == "__main__":
     # Test the function locally
     logger.info("Testing locally...")
     test_event = {
-        'resource': '/chat',
-        'path': '/chat',
-        'httpMethod': 'POST',
-        'headers': None,
-        'multiValueHeaders': None,
-        'queryStringParameters': None,
-        'multiValueQueryStringParameters': None,
-        'pathParameters': None,
-        'stageVariables': None,
-        'requestContext': {
-            'resourceId': 'resource-id',
-            'resourcePath': '/chat',
-            'httpMethod': 'POST',
-            'extendedRequestId': 'extended-request-id',
-            'requestTime': '04/Dec/2024:09:58:01 +0000',
-            'path': '/chat',
-            'accountId': 'account-id',
-            'protocol': 'HTTP/1.1',
-            'stage': 'test-invoke-stage',
-            'domainPrefix': 'testPrefix',
-            'requestTimeEpoch': 1733306281143,
-            'requestId': 'request-id',
-            'identity': {
-                'cognitoIdentityPoolId': None,
-                'cognitoIdentityId': None,
-                'apiKey': 'test-invoke-api-key',
-                'principalOrgId': None,
-                'cognitoAuthenticationType': None,
-                'userArn': 'arn:aws:iam::account-id:root',
-                'apiKeyId': 'test-invoke-api-key-id',
-                'userAgent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)',
-                'accountId': 'account-id',
-                'caller': 'account-id',
-                'sourceIp': 'test-invoke-source-ip',
-                'accessKey': 'access-key',
-                'cognitoAuthenticationProvider': None,
-                'user': 'account-id'
+        "resource": "/logs",
+        "path": "/logs",
+        "httpMethod": "GET",
+        "headers": None,
+        "multiValueHeaders": None,
+        "queryStringParameters": {"earth_date": "2012-08-09"},
+        "multiValueQueryStringParameters": {"earth_date": ["2012-08-09"]},
+        "pathParameters": None,
+        "stageVariables": None,
+        "requestContext": {
+            "resourceId": "nn9e51",
+            "resourcePath": "/logs",
+            "httpMethod": "GET",
+            "extendedRequestId": "CiH_sHqgIAMF8dg=",
+            "requestTime": "09/Dec/2024:16:52:52 +0000",
+            "path": "/logs",
+            "accountId": "056785171326",
+            "protocol": "HTTP/1.1",
+            "stage": "test-invoke-stage",
+            "domainPrefix": "testPrefix",
+            "requestTimeEpoch": 1733763172093,
+            "requestId": "79839435-2c8c-43a4-a8b2-5d6f8a4c579d",
+            "identity": {
+                "cognitoIdentityPoolId": None,
+                "cognitoIdentityId": None,
+                "apiKey": "test-invoke-api-key",
+                "principalOrgId": None,
+                "cognitoAuthenticationType": None,
+                "userArn": "arn:aws:iam::056785171326:user/August",
+                "apiKeyId": "test-invoke-api-key-id",
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
+                "accountId": "056785171326",
+                "caller": "AIDAQ2OFI6N7EB4BVXOHV",
+                "sourceIp": "test-invoke-source-ip",
+                "accessKey": "ASIAQ2OFI6N7DUVCPH3Q",
+                "cognitoAuthenticationProvider": None,
+                "user": "AIDAQ2OFI6N7EB4BVXOHV",
             },
-            'domainName': 'testPrefix.testDomainName',
-            'apiId': 'api-id'
+            "domainName": "testPrefix.testDomainName",
+            "apiId": "tmlg7yfb6l",
         },
-        'body': json.dumps({
-            "earth_date": "2012-08-09"
-        }),
-        'isBase64Encoded': False
+        "body": None,
+        "isBase64Encoded": False,
     }
     result = lambda_handler(test_event, None)
     logger.info(f"Result: {result}")
